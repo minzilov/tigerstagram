2723cbd879587ea6715fa680c580dad55edebc8f
minzilov <min22zzang98@gmail.com>
2020-02-13 17:47:15 +0900

    daily life

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import page.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',page.views.home,name='home'),
+    path('detail/<int:post_id>',page.views.detail,name='detail'),
+
 ]
--- page/models.py
+++ page/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) #밖에있는DB를참조할수있음
+    pub_date = models.DateTimeField('publish') #작성일자
+    content = models.TextField()
+
+
+    def __str__(self):
+        return self.title
