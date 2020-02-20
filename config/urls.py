43b283bf114aae6b02fd2d47bb98f0d43b7ad939
minzilov <min22zzang98@gmail.com>
2020-02-20 18:50:03 +0900

    tigerstagrem

--- config/urls.py
+++ config/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 import page.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',page.views.home,name='home'),
     path('detail/<int:post_id>',page.views.detail,name='detail'),
-
-]
+    path('new/', page.views.new, name='new'),
+] + static('/media/', document_root=settings.MEDIA_ROOT)
--- page/views.py
+++ page/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.utils import timezone
 from .models import Post
 
 def home(request):
@@ -9,3 +10,15 @@ def home(request):
 def detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     return render(request,'detail.html',{'post':post})
+
+def new(request):
+    if request.method == 'POST':
+        post = Post()
+        post.author = request.user
+        post.title = request.POST['title']
+        post.content = request.POST['content']
+        post.pub_date = timezone.datetime.now()
+        post.save()
+        return redirect('/detail/'+str(post.id))
+    else:
+        return render(request,'new.html')
